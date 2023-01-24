68285af2b63748b10c39ad24695b72bf874152f5
hana <eltishev2@gmail.com>
2023-01-25 01:29:36 +0300

    First homework

---
+++ lesson1/src/main/java/ru/geek/brains/TaskTracker.java
@@ -0,0 +1,124 @@
+package ru.geek.brains;
+
+public class TaskTracker extends Task {
+    Task[] tasks = new Task[10];
+    int length = 0;
+
+
+    public void addTask(Task task){
+        if (length < 10){
+            tasks[length] = task;
+            length++;
+        } else {
+            System.out.println("This tracker is full. Create a new one.");
+        }
+    }
+    public void printTasks(){
+        for (int i = 0; i < length; i++){
+            System.out.println(tasks[i]);
+        }
+        System.out.println();
+    }
+    public void deleteFirst(int id){
+        for (int i = 0; i < length; i++){
+            if (tasks[i].getId() == id){
+                tasks[i] = null;
+                break;
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            if (tasks[i] == null) {
+                for (int j = i; j < length - 1; j++) {
+                    tasks[j] = tasks[j + 1];
+                }
+            }
+        }
+        length--;
+    }
+    public void deleteFirst(String name){
+        for (int i = 0; i < length; i++){
+            if (tasks[i].getName().equals(name)){
+                tasks[i] = null;
+                break;
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            if (tasks[i] == null) {
+                for (int j = i; j < length - 1; j++) {
+                    tasks[j] = tasks[j + 1];
+                }
+            }
+        }
+        length--;
+    }
+    public void deleteLast(int id){
+        for (int i = 0; i < length; i++){
+            if (tasks[length - 1 - i].getId() == id){
+                tasks[length - 1 - i] = null;
+                break;
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            if (tasks[i] == null) {
+                for (int j = i; j < length - 1; j++) {
+                    tasks[j] = tasks[j + 1];
+                }
+            }
+        }
+        length--;
+    }
+    public void deleteLast(String name){
+        for (int i = 0; i < length; i++){
+            if (tasks[length - 1 - i].getName().equals(name)){
+                tasks[length - 1 - i] = null;
+                break;
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            if (tasks[i] == null) {
+                for (int j = i; j < length - 1; j++) {
+                    tasks[j] = tasks[j + 1];
+                }
+            }
+        }
+        length--;
+    }
+    public void deleteAll(int id) {
+        int deleted = 0;
+        for (int i = 0; i < length; i++) {
+            if (tasks[i].getId() == id) {
+                tasks[i] = null;
+                deleted++;
+            }
+        }
+        for (int c = 0; c < deleted; c++) {
+            for (int i = 0; i < length; i++) {
+                if (tasks[i] == null) {
+                    for (int j = i; j < length - 1; j++) {
+                        tasks[j] = tasks[j + 1];
+                    }
+                }
+            }
+        }
+        length -= deleted;
+    }
+    public void deleteAll(String name) {
+        int deleted = 0;
+        for (int i = 0; i < length; i++) {
+            if (tasks[i].getName().equals(name)) {
+                tasks[i] = null;
+                deleted++;
+            }
+        }
+        for (int c = 0; c < deleted; c++) {
+            for (int i = 0; i < length; i++) {
+                if (tasks[i] == null) {
+                    for (int j = i; j < length - 1; j++) {
+                        tasks[j] = tasks[j + 1];
+                    }
+                }
+            }
+        }
+        length -= deleted;
+    }
+}
---
+++ lesson2/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import opt.variant.Example1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Example1.secondary();
+        System.out.println( "Hello World!" );
+    }
+}
